085d08951bff2ca90e7ad5503e566409002bdcc8
Richard Dimalanta <chardmd.au@gmail.com>
2020-06-08 14:44:05 +1000

    changed to jpeg

--- src/externals/exportImage.js
+++ src/externals/exportImage.js
@@ -5,10 +5,10 @@ export default async () => {
     'AvatarGenerator-pngContainer'
   )[0]
   const canvas = await html2canvas(node)
-  const dataUrl = canvas.toDataURL()
+  const dataUrl = canvas.toDataURL('image/jpeg')
 
   const link = document.createElement('a')
-  link.download = 'avatar.png'
+  link.download = 'avatar.jpeg'
   link.href = dataUrl
 
   document.body.appendChild(link)
